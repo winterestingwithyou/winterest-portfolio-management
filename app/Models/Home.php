5e25b6a6a5a6f8438fe6cb10f86c0cc73fc2fded
winterestingwithyou <yudistiraadam3@gmail.com>
2025-06-07 22:50:46 +0700

    make home model

---
+++ app/Models/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Home extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'intro_id',
+        'intro_en',
+        'myself_id',
+        'myself_en',
+        'image'
+    ];
+}
---
+++ database/migrations/2025_06_07_152407_create_homes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('home', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('intro_id');
+            $table->text('intro_en');
+            $table->text('myself_id');
+            $table->text('myself_en');
+            $table->text('image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('home');
+    }
+};
